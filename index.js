93787c345d1890621c7ffd847524305a295cbda9
Muhammad Khalid Hussain <khalidks29@gmail.com>
2024-03-27 12:21:23 +0500

    Created Simple CLI Based Number Guessing Game
    Created Simple CLI Based Number Guessing Game

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguess",
+        type: "number",
+        message: [
+            `I have a number in my my mind. Can you guess it right?
+      "Enter a number between 1 to 6:`,
+        ],
+    },
+]);
+if (answer.userguess === randomnumber) {
+    console.log("Congratulations Your Guess is Right");
+}
+else
+    console.log("You Failed ! Please Try Again");
